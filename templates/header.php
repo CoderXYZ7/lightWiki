be5b61e53b077d56627756a4f3c50a306cc28acd
MrTheCats <roggioantonio001@proton.me>
2025-10-05 12:22:07 +0200

    Fix: Update logo path in header

--- templates/header.php
+++ templates/header.php
@@ -58,7 +58,7 @@
             <div class="nav-container">
                 <h1>
                   <a href="/">
-                    <img src="../public/assets/icons/mainLogo.png" alt="<?php echo htmlspecialchars(
+                    <img src="./assets/icons/mainLogo.png" alt="<?php echo htmlspecialchars(
                         $config["site_title"],
                     ); ?> logo" class="header-logo">
                   </a>
